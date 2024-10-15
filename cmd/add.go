70f6e7c617b970e934476a4c1016467ea246d2d4
Jacob Peyron <jacob.peyron@gmail.com>
2024-10-15 21:28:56 +0200

    wording

--- cmd/add.go
+++ cmd/add.go
@@ -53,7 +53,7 @@ func noteAdd(cmd *cobra.Command, args []string) {
 		quitError("db add", err)
 	}
 
-	fmt.Printf("Note created: %v\n", id)
+	fmt.Printf("Created note: %v\n", id)
 }
 
 func produceNote(args []string) string {
